ab3d9ff5f98e9c9f430860f663d3576152d2f2b2
Brainants PC <pc.brainants@gmail.com>
2020-05-31 22:48:14 +0545

    Initiated cloud function

---
+++ cloud_functions/functions/src/index.ts
@@ -0,0 +1,51 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+var rp = require("request-promise");
+
+export const runEveryMinute = functions.pubsub
+  .schedule("every minute")
+  .onRun(async () => {
+    // get the list of subscribers
+    const subscribers = await admin
+      .firestore()
+      .collection("subscriptions")
+      .get();
+
+    await Promise.all(
+      subscribers.docs
+        .map((d) => d.data())
+        .map((sub) => checkAndSendNotification)
+    );
+    console.log("send notificaions for energy");
+  });
+
+async function checkAndSendNotification(subscriber: any) {
+  const userStats = await getUsersStat(subscriber.apiKey);
+  const energy = userStats.energy;
+  if (energy.maximum == energy.current) {
+    return sendNotificaionToUser(
+      subscriber.token,
+      "Full Energy Bar",
+      "You have got full Energy bar, go spend on something."
+    );
+  }
+  return null;
+}
+
+async function getUsersStat(apiKey: string) {
+  return rp(`https://api.torn.com/user/?selections=bars&key=${apiKey}`);
+}
+
+async function sendNotificaionToUser(
+  token: string,
+  title: string,
+  body: string
+) {
+  return admin.messaging().send({
+    token: token,
+    notification: {
+      title: title,
+      body: body,
+    },
+  });
+}
