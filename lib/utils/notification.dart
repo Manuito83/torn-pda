8d480f70999a86694128949decf08996a2dd3a5b
Manuel Ortega <manuortega@gmail.com>
2020-07-04 23:49:20 +0200

    Improved notification settings

--- lib/utils/notification.dart
+++ lib/utils/notification.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:math';
-
+import 'dart:typed_data';
+import 'dart:ui';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:torn_pda/main.dart';
 
@@ -11,19 +12,32 @@ Future showNotification(Map payload) async {
 }
 
 Future showNotificationOnAndroid(Map payload) async {
-  var bigPictureAndroidStyle;
+  var vibrationPattern = Int64List(8);
+  vibrationPattern[0] = 0;
+  vibrationPattern[1] = 400;
+  vibrationPattern[2] = 400;
+  vibrationPattern[3] = 600;
+  vibrationPattern[4] = 400;
+  vibrationPattern[5] = 800;
+  vibrationPattern[6] = 400;
+  vibrationPattern[7] = 1000;
 
   var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-    "Alerts",
-    "Alerts",
-    "Alerts",
+    "Automatic alerts",
+    "Alerts Full",
+    "Automatic alerts chosen by the user",
     importance: Importance.Max,
     priority: Priority.High,
     visibility: NotificationVisibility.Public,
     autoCancel: true,
     channelShowBadge: true,
-    styleInformation: bigPictureAndroidStyle,
-    setAsGroupSummary: true,
+    icon: 'notification_icon',
+    sound: RawResourceAndroidNotificationSound('slow_spring_board'),
+    vibrationPattern: vibrationPattern,
+    enableLights: true,
+    ledColor: const Color.fromARGB(255, 255, 0, 0),
+    ledOnMs: 1000,
+    ledOffMs: 500,
     ticker: payload["notification"]["title"],
   );
 
@@ -43,7 +57,9 @@ Future showNotificationOnAndroid(Map payload) async {
 Future showNotificationOnIos(Map payload) async {
   var platformChannelSpecifics = NotificationDetails(
     null,
-    IOSNotificationDetails(),
+    IOSNotificationDetails(
+      sound: 'slow_spring_board.aiff',
+    ),
   );
 
   await flutterLocalNotificationsPlugin.show(
