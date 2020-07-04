a5208ccb16c229505ceb8163897c3f507706d460
Manuel Ortega <manuortega@gmail.com>
2020-07-04 23:48:25 +0200

    Added beta-testing notes

--- lib/pages/alerts.dart
+++ lib/pages/alerts.dart
@@ -21,74 +21,97 @@ class _AlertsSettingsState extends State<AlertsSettings> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Alerts'),
-        leading: new IconButton(
-          icon: new Icon(Icons.menu),
-          onPressed: () {
-            final ScaffoldState scaffoldState =
-                context.findRootAncestorStateOfType();
-            scaffoldState.openDrawer();
-          },
+    return Banner(
+      location: BannerLocation.bottomEnd,
+      message: 'BETA',
+      color: Colors.red.withOpacity(0.8),
+      textStyle: TextStyle(
+          fontWeight: FontWeight.w700, fontSize: 12.0, letterSpacing: 1.6),
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Alerts'),
+          leading: new IconButton(
+            icon: new Icon(Icons.menu),
+            onPressed: () {
+              final ScaffoldState scaffoldState =
+                  context.findRootAncestorStateOfType();
+              scaffoldState.openDrawer();
+            },
+          ),
         ),
-      ),
-      body: FutureBuilder(
-        future: _firestoreProfileReceived,
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.data is FirebaseUserModel) {
-              _firebaseUserModel = snapshot.data;
-              return Column(
-                children: [
-                  SizedBox(height: 20),
-                  CheckboxListTile(
-                    value: _firebaseUserModel.travelNotification ?? false,
-                    title: Text("Travel Arrival Notification"),
-                    subtitle: Text(
-                        "Get notified 60 seconds before you reach your destination"),
-                    onChanged: (value) {
-                      setState(() {
-                        _firebaseUserModel?.travelNotification = value;
-                      });
-                      firestore.subscribeToTravelNotification(value);
-                    },
-                  ),
-                  CheckboxListTile(
-                    value: _firebaseUserModel.energyNotification ?? false,
-                    title: Text("Energy Full Notification"),
-                    subtitle: Text("Get notified once you reach full energy"),
-                    onChanged: (value) {
-                      setState(() {
-                        _firebaseUserModel?.energyNotification = value;
-                      });
-                      firestore.subscribeToEnergyNotification(value);
-                    },
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Text(
-                      "Note: If you dont open the app for more than 7 days, "
-                          "all notifications will be turned off automatically. "
-                          "This is to prevent the over usage of our resources. "
-                          "Please make sure you return back to the app once a "
-                          "week to get uninterrupted notification service.",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText2
-                          .copyWith(height: 1.8, fontStyle: FontStyle.italic),
+        body: FutureBuilder(
+          future: _firestoreProfileReceived,
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.data is FirebaseUserModel) {
+                _firebaseUserModel = snapshot.data;
+                return Column(
+                  children: [
+                    SizedBox(height: 20),
+                    CheckboxListTile(
+                      checkColor: Colors.white,
+                      activeColor: Colors.blueGrey,
+                      value: _firebaseUserModel.travelNotification ?? false,
+                      title: Text("Travel Arrival Notification"),
+                      subtitle: Text(
+                          "Get notified just before you arrive"),
+                      onChanged: (value) {
+                        setState(() {
+                          _firebaseUserModel?.travelNotification = value;
+                        });
+                        firestore.subscribeToTravelNotification(value);
+                      },
                     ),
-                  )
-                ],
-              );
-            } else {
-              return _connectError();
-            }
+                    CheckboxListTile(
+                      checkColor: Colors.white,
+                      activeColor: Colors.blueGrey,
+                      value: _firebaseUserModel.energyNotification ?? false,
+                      title: Text("Energy Full Notification"),
+                      subtitle: Text("Get notified once you reach full energy"),
+                      onChanged: (value) {
+                        setState(() {
+                          _firebaseUserModel?.energyNotification = value;
+                        });
+                        firestore.subscribeToEnergyNotification(value);
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Text(
+                        "Note: if you don't open the app for more than 7 days, "
+                            "all notifications will be turned off automatically. "
+                            "This is to prevent the over usage of resources. "
+                            "Please make sure you return back to the app once a "
+                            "week to get uninterrupted service.",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            .copyWith(height: 1.3, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Text(
+                        "Note: this feature is under test. If you find an issue, "
+                            "please be kind and report it (see 'About' section).",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            .copyWith(height: 1.3, fontStyle: FontStyle.italic),
+                      ),
+                    )
+                  ],
+                );
+              } else {
+                return _connectError();
+              }
 
-          } else {
-            return Center(child: CircularProgressIndicator());
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
           }
-        }
+        ),
       ),
     );
   }
