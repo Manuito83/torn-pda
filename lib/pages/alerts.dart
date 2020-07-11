6d238e5898397ccd173d5ac1cf297f6d8c72756f
Manuel Ortega <manuortega@gmail.com>
2020-07-11 20:17:25 +0200

    Adapted alerts for v1.5.0 release

--- lib/pages/alerts.dart
+++ lib/pages/alerts.dart
@@ -10,7 +10,6 @@ class AlertsSettings extends StatefulWidget {
 }
 
 class _AlertsSettingsState extends State<AlertsSettings> {
-
   FirebaseUserModel _firebaseUserModel;
 
   Future _firestoreProfileReceived;
@@ -19,9 +18,8 @@ class _AlertsSettingsState extends State<AlertsSettings> {
   void initState() {
     super.initState();
     _firestoreProfileReceived = firestore.getUserProfile();
-    analytics.logEvent(
-        name: 'section_changed',
-        parameters: {'section': 'alerts'});
+    analytics
+        .logEvent(name: 'section_changed', parameters: {'section': 'alerts'});
   }
 
   @override
@@ -44,31 +42,58 @@ class _AlertsSettingsState extends State<AlertsSettings> {
               scaffoldState.openDrawer();
             },
           ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.info_outline,
+              ),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return _alertsInfoDialog();
+                  },
+                );
+              },
+            ),
+          ],
         ),
         body: FutureBuilder(
-          future: _firestoreProfileReceived,
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.data is FirebaseUserModel) {
-                _firebaseUserModel = snapshot.data;
-                return Column(
-                  children: [
-                    SizedBox(height: 20),
-                    CheckboxListTile(
-                      checkColor: Colors.white,
-                      activeColor: Colors.blueGrey,
-                      value: _firebaseUserModel.travelNotification ?? false,
-                      title: Text("Travel Arrival Notification"),
-                      subtitle: Text(
-                          "Get notified just before you arrive"),
-                      onChanged: (value) {
-                        setState(() {
-                          _firebaseUserModel?.travelNotification = value;
-                        });
-                        firestore.subscribeToTravelNotification(value);
-                      },
-                    ),
-                    CheckboxListTile(
+            future: _firestoreProfileReceived,
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.data is FirebaseUserModel) {
+                  _firebaseUserModel = snapshot.data;
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          "Alerts are automatic notifications that you only "
+                          "need to activate once. However, you will be notified "
+                          "earlier than with manual notifications.",
+                          style: Theme.of(context).textTheme.bodyText2.copyWith(
+                                height: 1.3,
+                              ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CheckboxListTile(
+                          checkColor: Colors.white,
+                          activeColor: Colors.blueGrey,
+                          value: _firebaseUserModel.travelNotification ?? false,
+                          title: Text("Travel Notification"),
+                          subtitle: Text("Get notified just before you arrive"),
+                          onChanged: (value) {
+                            setState(() {
+                              _firebaseUserModel?.travelNotification = value;
+                            });
+                            firestore.subscribeToTravelNotification(value);
+                          },
+                        ),
+                      ),
+                      /*CheckboxListTile(
                       checkColor: Colors.white,
                       activeColor: Colors.blueGrey,
                       value: _firebaseUserModel.energyNotification ?? false,
@@ -80,43 +105,16 @@ class _AlertsSettingsState extends State<AlertsSettings> {
                         });
                         firestore.subscribeToEnergyNotification(value);
                       },
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: Text(
-                        "Note: if you don't open the app for more than 7 days, "
-                            "all notifications will be turned off automatically. "
-                            "This is to prevent the over usage of resources. "
-                            "Please make sure you return back to the app once a "
-                            "week to get uninterrupted service.",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText2
-                            .copyWith(height: 1.3, fontStyle: FontStyle.italic),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: Text(
-                        "Note: this feature is under test. If you find an issue, "
-                            "please be kind and report it (see 'About' section).",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText2
-                            .copyWith(height: 1.3, fontStyle: FontStyle.italic),
-                      ),
-                    )
-                  ],
-                );
+                    ),*/
+                    ],
+                  );
+                } else {
+                  return _connectError();
+                }
               } else {
-                return _connectError();
+                return Center(child: CircularProgressIndicator());
               }
-
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          }
-        ),
+            }),
       ),
     );
   }
@@ -142,12 +140,60 @@ class _AlertsSettingsState extends State<AlertsSettings> {
             ),
           ),
           SizedBox(height: 20),
-          Text(
-              'If this problem reoccurs, please log out from Torn API (remove '
+          Text('If this problem reoccurs, please log out from Torn API (remove '
               'you API Key in the Settings section and insert it again. Sorry for '
               'the inconvenience!'),
         ],
       ),
+    );
+  }
+
+  Widget _alertsInfoDialog() {
+    return AlertDialog(
+      title: Text(
+        "Alerts",
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      ),
+      content: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "This feature is under test. If you find any issues, "
+                "please be kind and report it (see 'About' section).",
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Note: if you don't use Torn PDA for more than 7 days, "
+                "all notifications will be turned off automatically. "
+                "This is to prevent the over usage of resources. "
+                "Please make sure you return back to the app once a "
+                "week to get uninterrupted service.",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontStyle: FontStyle.italic
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        FlatButton(
+          child: Text("Close"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
     );
   }
 }
