883c5a57bbb1957e9df69edfdf21b53531a0d514
Manuel Ortega <manuortega@gmail.com>
2020-07-16 20:17:01 +0200

    Easy crimes initial setup

--- lib/widgets/webview2.dart
+++ lib/widgets/webview2.dart
@@ -26,9 +26,6 @@ class _WebView2State extends State<WebView2> {
     super.initState();
     _initialUrl = widget.customUrl;
     _pageTitle = widget.customTitle;
-
-    // TODO: remove after testing
-    print('TEST WebView used for $_initialUrl');
   }
 
   @override
@@ -59,24 +56,80 @@ class _WebView2State extends State<WebView2> {
             left: false,
             right: false,
             bottom: true,
-            child: InAppWebView(
-              initialUrl: _initialUrl,
-              initialHeaders: {},
-              initialOptions: InAppWebViewGroupOptions(
-                crossPlatform: InAppWebViewOptions(
-                  debuggingEnabled: true,
-                  preferredContentMode: UserPreferredContentMode.DESKTOP,
+            child: Column(
+              children: [
+                RaisedButton(
+                  child: Text('lala'),
+                  onPressed: () async {
+
+                    await webView.evaluateJavascript(source: """
+
+                      var first_load = true;
+                      
+                      if (first_load) {
+                        var loadingPlaceholderContent = `
+                          <div class="content-title m-bottom10">
+                            <h4 class="left">Crimes</h4>
+                            <hr class="page-head-delimiter">
+                            <div class="clear"></div>
+                          </div>`
+                          
+                        first_load = false;
+                      }
+                      
+                      loadingPlaceholderContent += `<img class="ajax-placeholder" src="/images/v2/main/ajax-loader.gif"/>`;
+                      
+                      window.location.hash = "#";
+                      \$(".content-wrapper").html(loadingPlaceholderContent);
+                      
+                      var action = 'https://www.torn.com/crimes.php?step=docrime2&timestamp=' + Date.now();
+                      
+                      ajaxWrapper({
+                        url: action,
+                        type: 'POST',
+                        data: 'nervetake=2&crime=searchtrainstation',
+                        oncomplete: function(resp) {
+                          \$(".content-wrapper").html(resp.responseText);
+                        
+                        var steps = action.split("?"),
+                        step = steps[1] ? steps[1].split("=")[1] : "";
+                        if (step == "docrime2" || step == "docrime4") refreshTopOfSidebar();
+                        if (animElement) clearTimeout(animElement);
+                        highlightElement("/" + step + ".php");
+                        },
+                        onerror: function(e) {
+                          console.error(e)
+                        }
+                      });
+
+                  """);
+                  },
                 ),
-                android: AndroidInAppWebViewOptions(
-                  useWideViewPort: true,
-                  loadWithOverviewMode: true,
-                  builtInZoomControls: true,
-                  displayZoomControls: true,
+                Expanded(
+                  child: InAppWebView(
+                    initialUrl: _initialUrl,
+                    initialHeaders: {},
+                    initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform: InAppWebViewOptions(
+                        debuggingEnabled: true,
+                        preferredContentMode: UserPreferredContentMode.DESKTOP,
+                      ),
+                      android: AndroidInAppWebViewOptions(
+                        useWideViewPort: true,
+                        loadWithOverviewMode: true,
+                        builtInZoomControls: true,
+                        displayZoomControls: true,
+                      ),
+                    ),
+                    onWebViewCreated: (InAppWebViewController controller) {
+                      webView = controller;
+                    },
+                    onConsoleMessage: (controller, consoleMessage) {
+                      print("CONSOLE MESSAGE: " + consoleMessage.message);
+                    },
+                  ),
                 ),
-              ),
-              onWebViewCreated: (InAppWebViewController controller) {
-                webView = controller;
-              },
+              ],
             ),
           ),
         ),
