beb60ca80a0937bf9d1128b903f67e8ebfe7722c
Manuel Ortega <manuortega@gmail.com>
2020-12-06 20:50:33 +0100

    Initial awards interface

--- lib/pages/awards_page.dart
+++ lib/pages/awards_page.dart
@@ -5,6 +5,27 @@ import 'package:torn_pda/providers/settings_provider.dart';
 import 'package:torn_pda/utils/external/yata_comm.dart';
 import 'package:torn_pda/providers/theme_provider.dart';
 import 'package:torn_pda/providers/user_details_provider.dart';
+import 'package:torn_pda/models/awards/awards_model.dart' as yata;
+import 'package:torn_pda/widgets/other/flipping_yata.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
+class Award {
+  Award({
+    this.name = "",
+    this.description = "",
+    this.type = "",
+    this.image = "",
+    this.achieve = 0,
+    this.daysLeft = 0,
+  });
+
+  String name;
+  String description;
+  String type;
+  String image;
+  double achieve;
+  double daysLeft;
+}
 
 class AwardsPage extends StatefulWidget {
   @override
@@ -12,7 +33,10 @@ class AwardsPage extends StatefulWidget {
 }
 
 class _AwardsPageState extends State<AwardsPage> {
-  Future _getInitialLootInformation;
+  // Main list with all awards
+  var _allAwards = List<Award>();
+
+  Future _getAwardsPayload;
   bool _apiSuccess = false;
 
   SettingsProvider _settingsProvider;
@@ -24,7 +48,7 @@ class _AwardsPageState extends State<AwardsPage> {
     super.initState();
     _settingsProvider = Provider.of<SettingsProvider>(context, listen: false);
     _userProvider = Provider.of<UserDetailsProvider>(context, listen: false);
-    _getInitialLootInformation = _fetchYataApi();
+    _getAwardsPayload = _fetchYataApi();
   }
 
   @override
@@ -44,7 +68,7 @@ class _AwardsPageState extends State<AwardsPage> {
             )
           : null,
       body: FutureBuilder(
-        future: _getInitialLootInformation,
+        future: _getAwardsPayload,
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (_apiSuccess) {
@@ -54,13 +78,10 @@ class _AwardsPageState extends State<AwardsPage> {
                   children: <Widget>[
                     Padding(
                       padding: const EdgeInsets.only(left: 15, top: 5),
-                      child: Text(
-                        'Test',
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                          fontStyle: FontStyle.italic,
-                          fontSize: 13,
-                        ),
+                      child: ListView(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        children: _allAwardsCards(),
                       ),
                     ),
                     SizedBox(height: 20),
@@ -71,7 +92,16 @@ class _AwardsPageState extends State<AwardsPage> {
               return _connectError();
             }
           } else {
-            return Center(child: CircularProgressIndicator());
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Fetching data...'),
+                  SizedBox(height: 30),
+                  FlippingYata(),
+                ],
+              ),
+            );
           }
         },
       ),
@@ -114,17 +144,88 @@ class _AwardsPageState extends State<AwardsPage> {
     );
   }
 
-  Future<dynamic> _fetchYataApi() async {
-    try {
+  List<Widget> _allAwardsCards() {
+    var awardsCards = List<Widget>();
 
-      var reply = await YataComm.getAwards(_userProvider.myUser.userApiKey);
+    for (var award in _allAwards) {
+      Color borderColor = Colors.transparent;
+      if (award.achieve == 1) {
+        borderColor = Colors.green;
+      } else if (award.achieve > 0.80 && award.achieve < 1) {
+        borderColor = Colors.blue;
+      }
 
-      print(reply);
-      print('test');
+      awardsCards.add(
+        Card(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: borderColor, width: 1.5),
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            child: Column(
+              children: [
+                Text(award.name),
+                Row(
+                  children: [
+                    Text("${(award.achieve * 100).round()}%"),
+                    SizedBox(width: 10),
 
-    } catch (e) {
-      return false;
+                    // TODO: show awarded time instead, if already achieved
+                    award.daysLeft != null
+                        ? Text("${(award.daysLeft).round()} days")
+                        : SizedBox.shrink(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
     }
-    return false;
+
+    return awardsCards;
+  }
+
+  Future _fetchYataApi() async {
+    var reply = await YataComm.getAwards(_userProvider.myUser.userApiKey);
+    if (reply is YataError) {
+      // TODO
+    } else {
+      await _populateInfo(reply);
+      _apiSuccess = true;
+    }
+  }
+
+  _populateInfo(yata.YataAwards awardsModel) {
+    var awardsMap = awardsModel.awards.toJson();
+
+    awardsMap.forEach((awardsType, awardValues) {
+      var awardsMap = awardValues as Map;
+
+      awardsMap.forEach((key, value) {
+        try {
+          var singleAward = Award(
+            type: awardsType,
+            name: value["name"],
+            description: value["description"],
+            image: value["img"] == null ? null : value["img"],
+            achieve: value["achieve"].toDouble(),
+            daysLeft: value["left"] == null
+                ? null
+                : value["left"] is String
+                    ? double.parse(value["left"])
+                    : value["left"].toDouble(),
+          );
+          _allAwards.add(singleAward);
+        } catch (e) {
+          // TODO activate and delete print
+          print(e);
+          /*          FirebaseCrashlytics.instance
+              .log("PDA Crash at YATA AWARD (${value["name"]}). Error: $e");
+          FirebaseCrashlytics.instance.recordError(e, null);*/
+        }
+      });
+    });
   }
 }
