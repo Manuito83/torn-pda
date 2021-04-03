2f5fd631da90316b8727ba2a2fdf8ed14f6f333b
Manuito83 <manuortega@gmail.com>
2021-04-03 18:47:52 +0200

    Functions for loot

--- cloud_functions/functions/src/index.ts
+++ cloud_functions/functions/src/index.ts
@@ -4,6 +4,7 @@ import { travelGroup } from "./travel_check";
 import { staleGroup } from "./stale_users"
 import { playersGroup } from "./players";
 import { foreignStocksGroup } from "./foreign_stocks";
+import { lootGroup } from "./loot";
 // import { testGroup } from "./tests";
 // import { helperGroup } from "./helpers";
 
@@ -19,5 +20,6 @@ export const travel = travelGroup
 export const stale = staleGroup;
 export const players = playersGroup;
 export const stocks = foreignStocksGroup;
+export const loot = lootGroup;
 // export const tests = testGroup;
 // export const helper = helperGroup;
