bd7d03c01a5992bea5b02c2c106169ceac1ed6e9
Manuito83 <manuortega@gmail.com>
2021-04-26 22:56:55 +0200

    Added new events

---
+++ lib/widgets/profile/event_icons.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class EventIcons extends StatelessWidget {
+  const EventIcons({
+    Key key,
+    @required this.message,
+  }) : super(key: key);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget insideIcon;
+    if (message.contains('revive')) {
+      insideIcon = Icon(
+        Icons.local_hospital,
+        color: Colors.green,
+        size: 20,
+      );
+    } else if (message.contains('the director of') ||
+        message.contains('You have been fired from') ||
+        message.contains('You application to join the company')) {
+      insideIcon = Icon(
+        Icons.work,
+        color: Colors.brown[300],
+        size: 20,
+      );
+    } else if (message
+        .contains('After becoming very ill, you decide to head')) {
+      insideIcon = Icon(
+        MdiIcons.radioactive,
+        color: Colors.amber[800],
+        size: 20,
+      );
+    } else if (message.contains('The territorial was between') ||
+        message.contains('has initiated an assault on') ||
+        message.contains('successfully assaulted')) {
+      insideIcon = Icon(
+        MdiIcons.fencing,
+        color: Colors.black,
+        size: 20,
+      );
+    } else if (message.contains('You are now known in the city as')) {
+      insideIcon = Icon(
+        MdiIcons.thoughtBubble,
+        color: Colors.black,
+        size: 20,
+      );
+    } else if (message.contains('jail') || message.contains('arrested you')) {
+      insideIcon = Center(
+        child: Image.asset(
+          'images/icons/jail.png',
+          width: 20,
+          height: 20,
+        ),
+      );
+    } else if (message.contains('trade')) {
+      insideIcon = Icon(
+        Icons.monetization_on,
+        color: Colors.green,
+        size: 20,
+      );
+    } else if (message.contains('has given you') ||
+        message.contains('You were sent') ||
+        message.contains('You have been credited with') ||
+        message.contains('on your doorstep')) {
+      insideIcon = Icon(
+        Icons.card_giftcard,
+        color: Colors.green,
+        size: 20,
+      );
+    } else if (message.contains('Get out of my education') ||
+        message.contains('You must have overdosed')) {
+      insideIcon = Icon(
+        Icons.warning,
+        color: Colors.red,
+        size: 20,
+      );
+    } else if (message.contains('purchased membership')) {
+      insideIcon = Icon(
+        Icons.fitness_center,
+        color: Colors.black54,
+        size: 20,
+      );
+    } else if (message.contains('You upgraded your level')) {
+      insideIcon = Icon(
+        Icons.file_upload,
+        color: Colors.green,
+        size: 20,
+      );
+    } else if (message.contains('The education course you were taking has')) {
+      insideIcon = Icon(
+        Icons.book,
+        color: Colors.brown,
+        size: 20,
+      );
+    } else if (message.contains('your proposal, you are now engaged')) {
+      insideIcon = Icon(
+        MdiIcons.ring,
+        color: Colors.amber[800],
+        size: 20,
+      );
+    } else if (message.contains('You divorced')) {
+      insideIcon = Icon(
+        MdiIcons.heartBroken,
+        color: Colors.red[800],
+        size: 20,
+      );
+    } else if (message.contains('won') ||
+        message.contains('lottery') ||
+        message.contains('check has been credited to your') ||
+        message.contains('withdraw your check from the bank') ||
+        message.contains('Your bank investment has ended') ||
+        message.contains('You were given \$')) {
+      insideIcon = Icon(
+        MdiIcons.cash100,
+        color: Colors.green,
+        size: 20,
+      );
+    } else if (message.contains('attacked you') ||
+        message.contains('mugged you and stole') ||
+        message.contains('attacked and hospitalized')) {
+      insideIcon = Container(
+        child: Center(
+          child: Image.asset(
+            'images/icons/ic_target_account_black_48dp.png',
+            width: 20,
+            height: 20,
+            color: Colors.red,
+          ),
+        ),
+      );
+    } else if (message.contains('You and your team') ||
+        message.contains('You have been selected') ||
+        message.contains('canceled the')) {
+      insideIcon = Container(
+        child: Center(
+          child: Image.asset(
+            'images/icons/ic_pistol_black_48dp.png',
+            width: 20,
+            height: 20,
+            color: Colors.blue,
+          ),
+        ),
+      );
+    } else if (message.contains('You left your faction') ||
+        message.contains('Your application to join the faction') ||
+        message.contains('has applied to join your faction')) {
+      insideIcon = Container(
+        child: Center(
+          child: Image.asset(
+            'images/icons/faction.png',
+            width: 15,
+            height: 15,
+            color: Colors.black,
+          ),
+        ),
+      );
+    } else if (message.contains('You came') ||
+        message.contains('race.') ||
+        message.contains('race and have received') ||
+        message.contains('Your best lap was')) {
+      insideIcon = Icon(
+        MdiIcons.gauge,
+        color: Colors.red[500],
+        size: 20,
+      );
+    } else if (message.contains('Your bug report')) {
+      insideIcon = Icon(
+        MdiIcons.bug,
+        color: Colors.red[500],
+        size: 20,
+      );
+    } else if (message.contains('You can begin programming a new virus')) {
+      insideIcon = Icon(
+        MdiIcons.virusOutline,
+        color: Colors.red[500],
+        size: 20,
+      );
+    } else if (message.contains('from your bazaar for')) {
+      insideIcon = Icon(
+        MdiIcons.store,
+        color: Colors.green,
+        size: 20,
+      );
+    } else if (message.contains('Your period of renting the') ||
+        message.contains('has sent an offer for you to rent') ||
+        message.contains('Your rental agreement with')) {
+      insideIcon = Icon(
+        Icons.house_outlined,
+        color: Colors.orange[900],
+        size: 20,
+      );
+    } else {
+      insideIcon = Container(
+        child: Center(
+          child: Text(
+            'T',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+          ),
+        ),
+      );
+    }
+    return insideIcon;
+  }
+}
