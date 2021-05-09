e35b35bfaf99680d74da2422c8dd6549492d9fd9
Manuito83 <manuortega@gmail.com>
2021-05-09 18:55:46 +0200

    Fixed try/catch to avoid a complete throw of the function and added alert for user with no refill selected

--- cloud_functions/functions/src/refills.ts
+++ cloud_functions/functions/src/refills.ts
@@ -20,8 +20,6 @@ export const refillsGroup = {
     const promises: Promise<any>[] = [];
     let errorUID = "";
 
-    try {
-  
       // Get the list of subscribers
       const response = await admin
         .firestore()
@@ -33,78 +31,111 @@ export const refillsGroup = {
       const subscribers = response.docs.map((d) => d.data());
       let ipBlocks = 0;
       let sent = 0;
+      let nothingSelected = 0;
       
+      // Loop all subscribers
       for(const key of Array.from(subscribers.keys()) ) {
-        const thisUser = subscribers[key];
-        errorUID = thisUser.uid;
         
-        const userRefills = await getUsersRefills(thisUser.apiKey);
-
-        // If API does not return error and refillsRequested field exists
-        if (!userRefills.error && thisUser.refillsRequested) {
-          let refillsToSend = [];
-
-          if (thisUser.refillsRequested.includes("energy") && !userRefills.refills.energy_refill_used) {
-              refillsToSend.push("energy");
-          }
+        // Try is inside the for loop so that individual issues does not throw the entire function
+        try {
+          const thisUser = subscribers[key];
+          errorUID = thisUser.uid;
+          
+          const apiRefills = await getUsersRefills(thisUser.apiKey);
   
-          if (thisUser.refillsRequested.includes("nerve") && !userRefills.refills.nerve_refill_used) {
-              refillsToSend.push("nerve");
-          }
-  
-          if (thisUser.refillsRequested.includes("token") && !userRefills.refills.token_refill_used) {
-              refillsToSend.push("casino tokens");
-          }
-  
-          let sendNotification = false;
-          let notificationTitle = "";
-          let notificationBody = "";
-          if (refillsToSend.length > 0) {
-              sendNotification = true;
-              if (refillsToSend.length === 1) {
-                  notificationTitle = "1 refill still available!";
-                  notificationBody = `You haven't used your ${refillsToSend} refill today. The day is almost over!`;
-              } else {
-                  notificationTitle = `${refillsToSend.length} refills still available!`;
-                  const last = refillsToSend.pop();
-                  const finalString = refillsToSend.join(', ') + ' and ' + last;
-                  notificationBody = `You haven't used your ${finalString} refills today. The day is almost over!`;
-              }
-          }
-  
-          if (sendNotification) {
-            promises.push(
-              sendNotificationToUser(
-                  thisUser.token,
-                  notificationTitle,
-                  notificationBody,
-                  "notification_refills",
-                  "#0000FF",
-                  "Alerts refills",
-                  "",
-                  "",
-                  thisUser.vibration,
-              )
-            );
+          // If API returns an error, will log it below (probably IP block?)
+          if (!apiRefills.error) {
             
-            sent++;
+            // Users that have not selected refills for the first time will have no refillsRequested array in Firestore
+            // Those that deselect all of them, will end up with an empty array
+            // In all these cases, we send a notification to remind them that they have nothing selected
+            if (!thisUser.refillsRequested || thisUser.refillsRequested.length === 0) {
+              
+              const emptyTitle = "No refills selected!";
+              const emptyBody = "You have activated your refills alerts but did not select which refills you would like to be notified about!"
+              
+              promises.push(
+                sendNotificationToUser(
+                    thisUser.token,
+                    emptyTitle,
+                    emptyBody,
+                    "notification_refills",
+                    "#0000FF",
+                    "Alerts refills",
+                    "",
+                    "",
+                    thisUser.vibration,
+                )
+              );
+              nothingSelected++;
+            
+            // This covers the normal case. User with an existing refillsRequested array that that is not empty 
+            // (the user has chosen at least 1 refill)
+            } else {
+
+              let refillsToSend = [];
+  
+              if (thisUser.refillsRequested.includes("energy") && !apiRefills.refills.energy_refill_used) {
+                  refillsToSend.push("energy");
+              }
+      
+              if (thisUser.refillsRequested.includes("nerve") && !apiRefills.refills.nerve_refill_used) {
+                  refillsToSend.push("nerve");
+              }
+      
+              if (thisUser.refillsRequested.includes("token") && !apiRefills.refills.token_refill_used) {
+                  refillsToSend.push("casino tokens");
+              }
+      
+              let sendNotification = false;
+              let notificationTitle = "";
+              let notificationBody = "";
+              if (refillsToSend.length > 0) {
+                  sendNotification = true;
+                  if (refillsToSend.length === 1) {
+                      notificationTitle = "1 refill still available!";
+                      notificationBody = `You haven't used your ${refillsToSend} refill today. The day is almost over!`;
+                  } else {
+                      notificationTitle = `${refillsToSend.length} refills still available!`;
+                      const last = refillsToSend.pop();
+                      const finalString = refillsToSend.join(', ') + ' and ' + last;
+                      notificationBody = `You haven't used your ${finalString} refills today. The day is almost over!`;
+                  }
+              }
+      
+              
+              if (sendNotification) {
+                promises.push(
+                  sendNotificationToUser(
+                      thisUser.token,
+                      notificationTitle,
+                      notificationBody,
+                      "notification_refills",
+                      "#0000FF",
+                      "Alerts refills",
+                      "",
+                      "",
+                      thisUser.vibration,
+                  )
+                );
+                sent++;
+              }
+            } 
+
+          } else if (apiRefills.error) {
+            // Return API errors for certain statistics
+            if (apiRefills.error.error.includes("IP block")) {
+              ipBlocks++;
+            }
           }
 
-        } else if (userRefills.error) {
-          // Return API errors for certain statistics
-          if (userRefills.error.error.includes("IP block")) {
-            ipBlocks++;
-          }
+        } catch (e) {
+          functions.logger.warn(`ERROR REFILL for ${errorUID}\n${e}`);
         }
       }
 
-      functions.logger.info(`Refills: ${subscribers.length} users, ${sent} sent, ${ipBlocks} blocks`);
-  
+      functions.logger.info(`Refills: ${subscribers.length} subscribed. ${sent} sent (+${nothingSelected} with no refills selected). ${ipBlocks} IP blocks.`);
       await Promise.all(promises);
-    
-    } catch (e) {
-      functions.logger.warn(`ERROR REFILL for ${errorUID}\n${e}`);
-    }
     
   }),
 
