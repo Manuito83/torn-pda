4a504128c2b1e5d594e232b3db14f1064e1ffefa
Manuito83 <manuortega@gmail.com>
2021-06-23 06:32:50 +0200

    Increased time out and added specific error code

--- lib/utils/api_caller.dart
+++ lib/utils/api_caller.dart
@@ -61,6 +61,11 @@ class ApiError {
   String errorReason;
   ApiError({int errorId}) {
     switch (errorId) {
+      // Torn PDA codes
+      case 100:
+        errorReason = 'connection timed out';
+        break;
+      // Torn codes
       case 0:
         errorReason = 'no connection';
         break;
@@ -104,6 +109,9 @@ class ApiError {
         break;
       case 13:
         errorReason = 'key is temporary disabled due to owner inactivity';
+        break;
+      case 14:
+        errorReason = 'daily read limit reached.';
         break;
     }
   }
@@ -509,7 +517,7 @@ class TornApiCaller {
     //url = 'http://www.google.com:81';  // DEBUG FOR TIMEOUT!
     //await new Future.delayed(const Duration(seconds : 5));  // DEBUG TIMEOUT 2
     try {
-      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
+      final response = await http.get(Uri.parse(url)).timeout(Duration(seconds: 20));
       if (response.statusCode == 200) {
         // Check if json is responding with errors
         var jsonResponse = json.decode(response.body);
@@ -521,7 +529,9 @@ class TornApiCaller {
       } else {
         return ApiError(errorId: 0);
       }
-    } catch (e) {
+    } on TimeoutException catch (_) {
+      return ApiError(errorId: 100);
+    } catch (_) {
       return ApiError(errorId: 0);
     }
   }
