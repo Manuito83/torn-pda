ffe9d91eef1c4cedc07911392e4994c012a526ab
Manuito83 <manuortega@gmail.com>
2021-08-07 02:13:26 +0200

    Added Company Activity and fixed conditional sources wildcards

--- lib/utils/changelog.dart
+++ lib/utils/changelog.dart
@@ -42,12 +42,14 @@ class _ChangeLogState extends State<ChangeLog> {
     var v2_5_0 = ChangeLogItem();
     v2_5_0.main.version = 'Torn PDA v2.5.0';
     v2_5_0.main.date = 'XX AUG 2021';
-    String feat2_5_0_1 = "Browser: added taps (visit the Tips sections for... tips)";
+    String feat2_5_0_1 = "Browser: added tabs (visit the Tips section for more information)";
     String feat2_5_0_2 = "Added pull to refresh to main browser (disabled by default)";
-    String feat2_5_0_3 = "Removed TAC";
+    String feat2_5_0_3 = "Added Company Activity userscript example";
+    String feat2_5_0_4 = "Removed Torn Attack Central (service discontinued)";
     v2_5_0.features.add(feat2_5_0_1);
     v2_5_0.features.add(feat2_5_0_2);
     v2_5_0.features.add(feat2_5_0_3);
+    v2_5_0.features.add(feat2_5_0_4);
 
     // VERSION 2.4.3
     var v2_4_3 = ChangeLogItem();
--- lib/utils/userscript_examples.dart
+++ lib/utils/userscript_examples.dart
@@ -9,6 +9,7 @@ class ScriptsExamples {
     exampleList.add(_racingPresetsExample());
     exampleList.add(_specialGymRatios());
     exampleList.add(_companyStocksOrderExample());
+    exampleList.add(_companyActivity());
     return exampleList;
   }
 
@@ -1625,6 +1626,60 @@ if (document.querySelector(".stock-list-wrap")) {
       urls: getUrls(source),
       name: "Company Stocks Order",
       exampleCode: 5,
+      edited: false,
+      source: source,
+    );
+
+  }
+
+  static UserScriptModel _companyActivity() {
+    var source = r"""// ==UserScript==
+// @name         Company Activity for Torn PDA
+// @namespace    TornExtensions
+// @version      1.0
+// @description  Shows the activity of employees.
+// @author       Twenu [XID 2659526]
+// @match        https://www.torn.com/companies.php*
+// @grant        none
+// ==/UserScript==
+  
+(function() {
+    //'use strict';
+    let APIKey = '###PDA-APIKEY###';
+    let targetNode = document.getElementById('employees');
+    let config = { childList: true };
+    let callback = function(mutationsList, observer) {
+        $("a.user.name").each(function() {
+            if($(this).closest("li").attr("data-user").length > 0 && $(this).next("img")) {
+                let uID = $(this).closest("li").attr("data-user");
+                let API = `https://api.torn.com/user/${uID}?selections=profile&key=${APIKey}`;
+                fetch(API)
+                  .then((res) => res.json())
+                  .then((res) => {
+                    $($($(this).parent().parent().parent()).find(".acc-body")).find(".stats").append('<span tabindex="0" class="span-cont t-first" aria-label="Active: "' + res.last_action.relative + '"><span class="bold t-show">Active:</span> ' + res.last_action.relative + '</span><span class="t-hide">/</span>');
+                    $($($(this).parent().parent().parent()).find(".acc-body")).find(".stats").append('<span tabindex="0" class="span-cont t-first" aria-label=" "><span class="bold t-show"></span></span> <span class="t-hide">/</span>');
+                    let days = res.last_action.relative.split(" ");
+                    if(days[1].includes("day"))
+                        if(parseInt(days[0]) == 1)
+                            $(this).parent().css("background-color", "orange");
+                        else if(parseInt(days[0]) >= 2)
+                            $(this).parent().css("background-color", "red");
+                  });
+            }
+        });
+    };
+    let observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+})();""";
+
+    return UserScriptModel(
+      // IMPORTANT: increment version by 1
+      version: 1,
+
+      enabled: false,
+      urls: getUrls(source),
+      name: "Company Activity",
+      exampleCode: 6,
       edited: false,
       source: source,
     );
