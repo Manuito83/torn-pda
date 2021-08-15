7355c4689a4a04c291190e10c6cf084110e2d66c
Manuito83 <manuortega@gmail.com>
2021-08-16 01:01:32 +0200

    Changed android distribution

--- cloud_functions/functions/src/alerts.ts
+++ cloud_functions/functions/src/alerts.ts
@@ -1,10 +1,12 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { sendEnergyNotification, sendNerveNotification, 
-  logTravelArrival, sendHospitalNotification, 
-  sendDrugsNotification, sendRacingNotification, 
-  sendMessagesNotification, sendEventsNotification, 
-  sendForeignRestockNotification, sendStockMarketNotification } from "./notification";
+import {
+  sendEnergyNotification, sendNerveNotification,
+  logTravelArrival, sendHospitalNotification,
+  sendDrugsNotification, sendRacingNotification,
+  sendMessagesNotification, sendEventsNotification,
+  sendForeignRestockNotification, sendStockMarketNotification
+} from "./notification";
 import { getUsersStat } from "./torn_api";
 
 const privateKey = require("../key/torn_key");
@@ -27,196 +29,196 @@ export const alertsGroup = {
   //*********** iOS ************//
   //****************************//
   checkIOS: functions.region('us-east4')
-  .runWith(runtimeOpts)
-  .pubsub
-  .schedule("0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57 * * * *")
-  .onRun(async () => {
+    .runWith(runtimeOpts)
+    .pubsub
+    .schedule("0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57 * * * *")
+    .onRun(async () => {
 
-    const promisesGlobal: Promise<any>[] = [];
+      const promisesGlobal: Promise<any>[] = [];
 
-    const millisAtStart = Date.now();
+      const millisAtStart = Date.now();
 
-    // Get stock market
-    const stockMarket = await getStockMarket(privateKey.tornKey);
+      // Get stock market
+      const stockMarket = await getStockMarket(privateKey.tornKey);
 
-    // Get existing stocks from Realtime DB
-    const firebaseAdmin = require("firebase-admin");
-    const db = firebaseAdmin.database();
-    const stocksDB = db.ref("stocks/restocks");
-    const foreignStocks = {};
-    await stocksDB.once("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+      // Get existing stocks from Realtime DB
+      const firebaseAdmin = require("firebase-admin");
+      const db = firebaseAdmin.database();
+      const stocksDB = db.ref("stocks/restocks");
+      const foreignStocks = {};
+      await stocksDB.once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+        });
       });
-    });
 
-    async function checkIOS() {
-      const promises: Promise<any>[] = [];
+      async function checkIOS() {
+        const promises: Promise<any>[] = [];
 
-      // Get the list of subscribers
-      const response = await admin
-        .firestore()
-        .collection("players")
-        .where("active", "==", true)
-        .where("alertsEnabled", "==", true)
-        .where("platform", "==", "ios")
-        .get();
+        // Get the list of subscribers
+        const response = await admin
+          .firestore()
+          .collection("players")
+          .where("active", "==", true)
+          .where("alertsEnabled", "==", true)
+          .where("platform", "==", "ios")
+          .get();
 
-      const subscribers = response.docs.map((d) => d.data());
-      let iOSBlocks = 0;
-      for(const key of Array.from(subscribers.keys()) ) {
-        promises.push(
-          sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function(value) {
-            if (value === "ip-block") {
-              iOSBlocks++;
-            }
-          })
-        );
+        const subscribers = response.docs.map((d) => d.data());
+        let iOSBlocks = 0;
+        for (const key of Array.from(subscribers.keys())) {
+          promises.push(
+            sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function (value) {
+              if (value === "ip-block") {
+                iOSBlocks++;
+              }
+            })
+          );
+        }
+
+        return Promise.all(promises).then(function (value) {
+          const millisAfterFinish = Date.now();
+          const difference = (millisAfterFinish - millisAtStart) / 1000;
+          functions.logger.info(`iOS: ${subscribers.length}. Blocks: ${iOSBlocks}. Time: ${difference}`);
+          return value;
+        });
       }
 
-      return Promise.all(promises).then(function(value) {
-        const millisAfterFinish = Date.now();
-        const difference = (millisAfterFinish - millisAtStart) / 1000;
-        functions.logger.info(`iOS: ${subscribers.length}. Blocks: ${iOSBlocks}. Time: ${difference}`);
-        return value;
-      });
-    }
-
-    promisesGlobal.push(checkIOS());
-    await Promise.all(promisesGlobal);
-  }),
+      promisesGlobal.push(checkIOS());
+      await Promise.all(promisesGlobal);
+    }),
 
 
   //****************************//
   //******* ANDROID LOW ********//
   //****************************//
   checkAndroidLow: functions.region('us-east4')
-  .runWith(runtimeOpts)
-  .pubsub
-  .schedule("1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55,58 * * * *")
-  .onRun(async () => {
+    .runWith(runtimeOpts)
+    .pubsub
+    .schedule("1,4,7,10,13,16,19,22,25,28,31,34,37,40,43,46,49,52,55,58 * * * *")
+    .onRun(async () => {
 
-    const promisesGlobal: Promise<any>[] = [];
+      const promisesGlobal: Promise<any>[] = [];
 
-    const millisAtStart = Date.now();
+      const millisAtStart = Date.now();
 
-    // Get stock market
-    const stockMarket = await getStockMarket(privateKey.tornKey);
+      // Get stock market
+      const stockMarket = await getStockMarket(privateKey.tornKey);
 
-    // Get existing stocks from Realtime DB
-    const firebaseAdmin = require("firebase-admin");
-    const db = firebaseAdmin.database();
-    const stocksDB = db.ref("stocks/restocks");
-    const foreignStocks = {};
-    await stocksDB.once("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+      // Get existing stocks from Realtime DB
+      const firebaseAdmin = require("firebase-admin");
+      const db = firebaseAdmin.database();
+      const stocksDB = db.ref("stocks/restocks");
+      const foreignStocks = {};
+      await stocksDB.once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+        });
       });
-    });
 
-    async function checkAndroidLow() {
-      const promises: Promise<any>[] = [];
-  
-      // Get the list of subscribers
-      const response = await admin
-        .firestore()
-        .collection("players")
-        .where("active", "==", true)
-        .where("alertsEnabled", "==", true)
-        .where("platform", "==", "android")
-        .where("level", "<", 35)
-        .get();
-      
-      const subscribers = response.docs.map((d) => d.data());
-      let androidLow = 0;
-      for(const key of Array.from(subscribers.keys()) ) {
-        promises.push(
-          sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function(value) {
-            if (value === "ip-block") {
-              androidLow++;
-            }
-          })
-        );
+      async function checkAndroidLow() {
+        const promises: Promise<any>[] = [];
+
+        // Get the list of subscribers
+        const response = await admin
+          .firestore()
+          .collection("players")
+          .where("active", "==", true)
+          .where("alertsEnabled", "==", true)
+          .where("platform", "==", "android")
+          .where("level", "<", 40)
+          .get();
+
+        const subscribers = response.docs.map((d) => d.data());
+        let androidLow = 0;
+        for (const key of Array.from(subscribers.keys())) {
+          promises.push(
+            sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function (value) {
+              if (value === "ip-block") {
+                androidLow++;
+              }
+            })
+          );
+        }
+
+        return Promise.all(promises).then(function (value) {
+          const millisAfterFinish = Date.now();
+          const difference = (millisAfterFinish - millisAtStart) / 1000;
+          functions.logger.info(`Android Low: ${subscribers.length}. Blocks: ${androidLow}. Time: ${difference}`);
+          return value;
+        });
       }
-  
-      return Promise.all(promises).then(function(value) {
-        const millisAfterFinish = Date.now();
-        const difference = (millisAfterFinish - millisAtStart) / 1000;
-        functions.logger.info(`Android Low: ${subscribers.length}. Blocks: ${androidLow}. Time: ${difference}`);
-        return value;
-      });
-    }
 
-    promisesGlobal.push(checkAndroidLow());
-    await Promise.all(promisesGlobal);
-  }),
+      promisesGlobal.push(checkAndroidLow());
+      await Promise.all(promisesGlobal);
+    }),
 
 
   //****************************//
   //******* ANDROID HIGH *******//
   //****************************//
   checkAndroidHigh: functions.region('us-east4')
-  .runWith(runtimeOpts)
-  .pubsub
-  .schedule("2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59 * * * *")
-  .onRun(async () => {
+    .runWith(runtimeOpts)
+    .pubsub
+    .schedule("2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59 * * * *")
+    .onRun(async () => {
 
-    const promisesGlobal: Promise<any>[] = [];
+      const promisesGlobal: Promise<any>[] = [];
 
-    const millisAtStart = Date.now();
+      const millisAtStart = Date.now();
 
-    // Get stock market
-    const stockMarket = await getStockMarket(privateKey.tornKey);
+      // Get stock market
+      const stockMarket = await getStockMarket(privateKey.tornKey);
 
-    // Get existing stocks from Realtime DB
-    const firebaseAdmin = require("firebase-admin");
-    const db = firebaseAdmin.database();
-    const stocksDB = db.ref("stocks/restocks");
-    const foreignStocks = {};
-    await stocksDB.once("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+      // Get existing stocks from Realtime DB
+      const firebaseAdmin = require("firebase-admin");
+      const db = firebaseAdmin.database();
+      const stocksDB = db.ref("stocks/restocks");
+      const foreignStocks = {};
+      await stocksDB.once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+        });
       });
-    });
 
-    async function checkAndroidHigh() {
-      const promises: Promise<any>[] = [];
-  
-      // Get the list of subscribers
-      const response = await admin
-        .firestore()
-        .collection("players")
-        .where("active", "==", true)
-        .where("alertsEnabled", "==", true)
-        .where("platform", "==", "android")
-        .where("level", ">=", 35)
-        .get();
-        
-      const subscribers = response.docs.map((d) => d.data());
-      let androidHigh = 0;
-      for(const key of Array.from(subscribers.keys()) ) {
-        promises.push(
-          sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function(value) {
-            if (value === "ip-block") {
-              androidHigh++;
-            }
-          })
-        );
+      async function checkAndroidHigh() {
+        const promises: Promise<any>[] = [];
+
+        // Get the list of subscribers
+        const response = await admin
+          .firestore()
+          .collection("players")
+          .where("active", "==", true)
+          .where("alertsEnabled", "==", true)
+          .where("platform", "==", "android")
+          .where("level", ">=", 40)
+          .get();
+
+        const subscribers = response.docs.map((d) => d.data());
+        let androidHigh = 0;
+        for (const key of Array.from(subscribers.keys())) {
+          promises.push(
+            sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function (value) {
+              if (value === "ip-block") {
+                androidHigh++;
+              }
+            })
+          );
+        }
+
+        return Promise.all(promises).then(function (value) {
+          const millisAfterFinish = Date.now();
+          const difference = (millisAfterFinish - millisAtStart) / 1000;
+          functions.logger.info(`Android High: ${subscribers.length}. Blocks: ${androidHigh}. Time: ${difference}`);
+          return value;
+        });
       }
-  
-      return Promise.all(promises).then(function(value) {
-        const millisAfterFinish = Date.now();
-        const difference = (millisAfterFinish - millisAtStart) / 1000;
-        functions.logger.info(`Android High: ${subscribers.length}. Blocks: ${androidHigh}. Time: ${difference}`);
-        return value;
-      });
-    }
 
 
-    // FOR TESTING
-    promisesGlobal.push(checkAndroidHigh());
-    await Promise.all(promisesGlobal);
-  }),
+      // FOR TESTING
+      promisesGlobal.push(checkAndroidHigh());
+      await Promise.all(promisesGlobal);
+    }),
 
 };
 
@@ -227,64 +229,64 @@ export const alertsGroup = {
 export const alertsTestGroup = {
 
   checkManuito: functions.region('us-east4')
-  .runWith(runtimeOpts)
-  .pubsub
-  .schedule("*/3 * * * *")
-  .onRun(async () => {
+    .runWith(runtimeOpts)
+    .pubsub
+    .schedule("*/3 * * * *")
+    .onRun(async () => {
 
-    const promisesGlobal: Promise<any>[] = [];
+      const promisesGlobal: Promise<any>[] = [];
 
-    const millisAtStart = Date.now();
+      const millisAtStart = Date.now();
 
-    // Get stock market
-    const stockMarket = await getStockMarket(privateKey.tornKey);
+      // Get stock market
+      const stockMarket = await getStockMarket(privateKey.tornKey);
 
-    // Get existing stocks from Realtime DB
-    const firebaseAdmin = require("firebase-admin");
-    const db = firebaseAdmin.database();
-    const stocksDB = db.ref("stocks/restocks");
-    const foreignStocks = {};
-    await stocksDB.once("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+      // Get existing stocks from Realtime DB
+      const firebaseAdmin = require("firebase-admin");
+      const db = firebaseAdmin.database();
+      const stocksDB = db.ref("stocks/restocks");
+      const foreignStocks = {};
+      await stocksDB.once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          foreignStocks[childSnapshot.val().codeName] = childSnapshot.val();
+        });
       });
-    });
 
-    async function checkManuito() {
-      const promises: Promise<any>[] = [];
+      async function checkManuito() {
+        const promises: Promise<any>[] = [];
 
-      // Get the list of subscribers
-      const response = await admin
-        .firestore()
-        .collection("players")
-        .where("active", "==", true)
-        .where("alertsEnabled", "==", true)
-        .where("name", "==", "Manuito")
-        .get();
+        // Get the list of subscribers
+        const response = await admin
+          .firestore()
+          .collection("players")
+          .where("active", "==", true)
+          .where("alertsEnabled", "==", true)
+          .where("name", "==", "Manuito")
+          .get();
 
-      const subscribers = response.docs.map((d) => d.data());
-      let manuitoBlocks = 0;
-      for(const key of Array.from(subscribers.keys()) ) {
-        promises.push(
-          sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function(value) {
-            if (value === "ip-block") {
-              manuitoBlocks++;
-            }
-          })
-        );
+        const subscribers = response.docs.map((d) => d.data());
+        let manuitoBlocks = 0;
+        for (const key of Array.from(subscribers.keys())) {
+          promises.push(
+            sendNotificationForProfile(subscribers[key], foreignStocks, stockMarket).then(function (value) {
+              if (value === "ip-block") {
+                manuitoBlocks++;
+              }
+            })
+          );
+        }
+
+        return Promise.all(promises).then(function (value) {
+          const millisAfterFinish = Date.now();
+          const difference = (millisAfterFinish - millisAtStart) / 1000;
+          functions.logger.info(`Manuito: ${subscribers.length}. Blocks: ${manuitoBlocks}. Time: ${difference}`);
+          return value;
+        });
       }
 
-      return Promise.all(promises).then(function(value) {
-        const millisAfterFinish = Date.now();
-        const difference = (millisAfterFinish - millisAtStart) / 1000;
-        functions.logger.info(`Manuito: ${subscribers.length}. Blocks: ${manuitoBlocks}. Time: ${difference}`);
-        return value;
-      });
-    }
-    
-    promisesGlobal.push(checkManuito());
-    await Promise.all(promisesGlobal);
-  }),
+      promisesGlobal.push(checkManuito());
+      await Promise.all(promisesGlobal);
+    }),
 };
 
 async function sendNotificationForProfile(subscriber: any, foreignStocks: any, stockMarket: any): Promise<any> {
@@ -320,14 +322,14 @@ async function sendNotificationForProfile(subscriber: any, foreignStocks: any, s
       await Promise.all(promises);
 
     } else {
-      
+
       // Return API errors for certain statistics
       if (userStats.error.error.includes("IP block")) {
         return "ip-block";
       }
-      
+
     }
-    
+
   } catch (e) {
     functions.logger.warn(`ERROR ALERTS \n${subscriber.uid} \n${e}`);
 
