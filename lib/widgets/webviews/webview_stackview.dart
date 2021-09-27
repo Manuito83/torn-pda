413d642fd70c86eb09faa12255308360a4f80ea9
Manuito83 <manuortega@gmail.com>
2021-09-27 09:23:19 +0200

    Fixed jail and hospital pagination icons

--- lib/widgets/webviews/webview_stackview.dart
+++ lib/widgets/webviews/webview_stackview.dart
@@ -430,6 +430,10 @@ class _WebViewStackViewState extends State<WebViewStackView> with TickerProvider
       return ImageIcon(AssetImage('images/icons/home/forums.png'));
     } else if (url.contains("yata.yt")) {
       return Image.asset('images/icons/yata_logo.png');
+    } else if (url.contains("jailview.php")) {
+      return Image.asset('images/icons/map/jail.png');
+    } else if (url.contains("hospitalview.php")) {
+      return Image.asset('images/icons/map/hospital.png');
     } else if (url.contains("events.php")) {
       return Image.asset('images/icons/home/events.png', color: _themeProvider.mainText);
     } else if (url.contains("properties.php")) {
