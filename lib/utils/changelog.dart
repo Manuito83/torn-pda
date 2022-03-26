eb8c75283d25ecd1652f02388b43dcf231a65ddc
Manuito83 <manuortega@gmail.com>
2022-03-26 11:51:44 +0100

    Added time options for refill alerts

--- lib/utils/changelog.dart
+++ lib/utils/changelog.dart
@@ -42,14 +42,16 @@ class _ChangeLogState extends State<ChangeLog> {
     var v2_7_0 = ChangeLogItem();
     v2_7_0.main.version = 'Torn PDA v2.7.0';
     v2_7_0.main.date = '10 APR 2022';
-    String feat2_7_0_1 = "Added quick items in faction armoury";
-    String feat2_7_0_2 = "The chaining browser now opens as a tab and incorporates all widgets";
-    String feat2_7_0_3 = "Corrected white flash when using browser with dark themes";
-    String feat2_7_0_4 = "Corrected issues with war targets cards and total stats calculation";
+    String feat2_7_0_1 = "Browser: added quick items widget in faction armoury";
+    String feat2_7_0_2 = "Chaining: the browser now opens as a tab and incorporates all widgets";
+    String feat2_7_0_3 = "Alerts: increased time options for refills";
+    String feat2_7_0_4 = "Fixed white flash when using browser with dark themes";
+    String feat2_7_0_5 = "Fixed issues with war targets cards and total stats calculation";
     v2_7_0.features.add(feat2_7_0_1);
     v2_7_0.features.add(feat2_7_0_2);
     v2_7_0.features.add(feat2_7_0_3);
     v2_7_0.features.add(feat2_7_0_4);
+    v2_7_0.features.add(feat2_7_0_5);
 
     // VERSION 2.6.7
     var v2_6_7 = ChangeLogItem();
