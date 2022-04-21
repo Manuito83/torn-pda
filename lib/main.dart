4730531c9967f74e83b00e36caba17ecbcf9b9b6
Manuito83 <manuortega@gmail.com>
2022-04-21 18:13:20 +0200

    Android versioning

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,8 @@ import 'package:torn_pda/utils/shared_prefs.dart';
 
 // TODO: CONFIGURE FOR APP RELEASE, include exceptions in Drawer if applicable
 const String appVersion = '2.7.0';
-const String androidVersion = '204';
-const String iosVersion = '212';
+const String androidVersion = '205';
+const String iosVersion = '213';
 
 final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
