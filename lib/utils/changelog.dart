d0d7ede3087e17fd69732373c58b129fe95d4f68
Manuito83 <manuortega@gmail.com>
2023-04-29 23:41:22 +0200

    Updated changelog

--- lib/utils/changelog.dart
+++ lib/utils/changelog.dart
@@ -49,14 +49,16 @@ class _ChangeLogState extends State<ChangeLog> {
     String feat3_1_0_2 = "Added Ranked War information and notifications to Profile page";
     String feat3_1_0_3 = "Shortcuts page and all related options have been moved to Settings";
     String feat3_1_0_4 = "Browser long-press menu now also adds a shortcut to the target link";
-    String feat3_1_0_5 = "Fixed issues redirecting to attack pages in certain conditions";
-    String feat3_1_0_6 = "Fixed issues when launching the app from a notification";
+    String feat3_1_0_5 = "Full update in War now updates all members faster";
+    String feat3_1_0_6 = "Fixed issues redirecting to attack pages in certain conditions";
+    String feat3_1_0_7 = "Fixed issues when launching the app from a notification";
     if (Platform.isAndroid) v3_1_0.features.add(feat3_1_0_1);
     v3_1_0.features.add(feat3_1_0_2);
     v3_1_0.features.add(feat3_1_0_3);
     v3_1_0.features.add(feat3_1_0_4);
     v3_1_0.features.add(feat3_1_0_5);
     v3_1_0.features.add(feat3_1_0_6);
+    v3_1_0.features.add(feat3_1_0_7);
 
     // VERSION 3.0.2
     var v3_0_2 = ChangeLogItem();
