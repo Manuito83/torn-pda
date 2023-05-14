f804d08131201541e9b24752af13d295f09a9138
Manuito83 <manuortega@gmail.com>
2023-05-14 23:25:31 +0200

    Android versioning

--- lib/main.dart
+++ lib/main.dart
@@ -54,8 +54,8 @@ import 'package:torn_pda/utils/shared_prefs.dart';
 
 // TODO: CONFIGURE FOR APP RELEASE, include exceptions in Drawer if applicable
 const String appVersion = '3.1.0';
-const String androidCompilation = '307';
-const String iosCompilation = '307';
+const String androidCompilation = '308';
+const String iosCompilation = '308';
 
 final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
--- lib/utils/changelog.dart
+++ lib/utils/changelog.dart
@@ -39,7 +39,7 @@ class _ChangeLogState extends State<ChangeLog> {
   void _createItems() {
     var itemList = <ChangeLogItem>[];
 
-    // Build 307 - 14/05/2023
+    // Build 308 - 14/05/2023
 
     // VERSION 3.1.0
     var v3_1_0 = ChangeLogItem();
