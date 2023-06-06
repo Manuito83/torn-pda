af21855f538512eee0b9fc1f331b4206d19eef1a
Manuito <manuortega@gmail.com>
2023-06-06 21:42:00 +0200

    Fixed quick actions iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,27 +14,19 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
-  // Quick_actions fix, refer to https://github.com/flutter/flutter/issues/13634
-  @available(iOS 9.0, *)
-  override func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
-  let controller = window.rootViewController as? FlutterViewController
-  let channel = FlutterMethodChannel(name: "plugins.flutter.io/quick_actions", binaryMessenger: controller! as! FlutterBinaryMessenger)
-  channel.invokeMethod("launch", arguments: shortcutItem.type)
-  }
     
-    /*
-    override func applicationDidBecomeActive(_ application: UIApplication) {
-        if #available(iOS 14.0, *) {
-            WidgetCenter.shared.getCurrentConfigurations { (result) in
-                guard let widgets = try? result.get() else { return }
-                widgets.forEach { (widget) in
-                    print(widget.debugDescription)
-                }
-            }
+  /*
+  override func applicationDidBecomeActive(_ application: UIApplication) {
+    if #available(iOS 14.0, *) {
+      WidgetCenter.shared.getCurrentConfigurations { (result) in
+        guard let widgets = try? result.get() else { return }
+        widgets.forEach { (widget) in
+          print(widget.debugDescription)
         }
+      }
     }
-    */
+  }
+  */
 }
 
 // Flutter 2.2.3 USB keyboard fix
