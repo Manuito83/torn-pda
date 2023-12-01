ee569946eb0a356023026458b874986fbcb69bd9
Manuito83 <manuortega@gmail.com>
2023-12-01 14:23:36 +0100

    Added additional deep link

--- lib/main.dart
+++ lib/main.dart
@@ -53,8 +53,8 @@ import 'package:workmanager/workmanager.dart';
 
 // TODO: CONFIGURE FOR APP RELEASE, include exceptions in Drawer if applicable
 const String appVersion = '3.2.2';
-const String androidCompilation = '358';
-const String iosCompilation = '358';
+const String androidCompilation = '359';
+const String iosCompilation = '359';
 
 final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
