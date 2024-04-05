2168332c9fa7f458da76ef28c53a106ab3cabd19
Manuito <manuortega@gmail.com>
2024-04-06 00:10:23 +0200

    Fixex Company Activity example user script

--- lib/main.dart
+++ lib/main.dart
@@ -58,8 +58,8 @@ import 'package:workmanager/workmanager.dart';
 
 // TODO (App release)
 const String appVersion = '3.3.2';
-const String androidCompilation = '398';
-const String iosCompilation = '398';
+const String androidCompilation = '399';
+const String iosCompilation = '399';
 
 // TODO (App release)
 const bool pointFunctionsEmulatorToLocal = false;
