c6ac86e4c5fd4ef86e3dbd7bc5fccedca38e2b56
Manuito <manuortega@gmail.com>
2024-05-21 19:08:53 +0200

    iOS versioning and further fixes for custom shortcuts

--- lib/main.dart
+++ lib/main.dart
@@ -58,8 +58,8 @@ import 'package:workmanager/workmanager.dart';
 
 // TODO (App release)
 const String appVersion = '3.4.2';
-const String androidCompilation = '420';
-const String iosCompilation = '420';
+const String androidCompilation = '421';
+const String iosCompilation = '421';
 
 // TODO (App release)
 const bool pointFunctionsEmulatorToLocal = false;
