b8c24bc1d91325134edd9dcad771256779a51a26
Manuito <manuortega@gmail.com>
2024-11-22 19:58:43 +0100

    Beta versioning

--- lib/main.dart
+++ lib/main.dart
@@ -68,8 +68,8 @@ import 'package:workmanager/workmanager.dart';
 
 // TODO (App release)
 const String appVersion = '3.6.0';
-const String androidCompilation = '457';
-const String iosCompilation = '457';
+const String androidCompilation = '458';
+const String iosCompilation = '458';
 
 // TODO (App release)
 // Note: if using Windows and calling HTTP functions, we need to change the URL in [firebase_functions.dart]
