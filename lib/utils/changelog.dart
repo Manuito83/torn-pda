c47b3d7dd1df8cdf38cdeb8dc1cb22119deb6e7e
Manuito <manuortega@gmail.com>
2025-08-20 16:35:56 +0200

    Package update and versioning

--- lib/utils/changelog.dart
+++ lib/utils/changelog.dart
@@ -100,7 +100,7 @@ class ChangeLogState extends State<ChangeLog> {
   void _createItems() {
     final itemList = <ChangeLogItem>[];
 
-    // v3.8.4 - Build 571 - 19/08/2025
+    // v3.8.4 - Build 572 - 20/08/2025
     itemList.add(
       ChangeLogItem()
         ..version = 'Torn PDA v3.8.4'
