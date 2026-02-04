3c251ce799998355762d8cae9779642e47acf6e4
Manuito <manuortega@gmail.com>
2026-02-04 18:32:04 +0100

    Beta versioning

--- lib/main.dart
+++ lib/main.dart
@@ -75,8 +75,8 @@ import 'package:workmanager/workmanager.dart';
 
 // TODO (App release)
 const String appVersion = '3.11.1';
-const String androidCompilation = '614';
-const String iosCompilation = '614';
+const String androidCompilation = '615';
+const String iosCompilation = '615';
 
 /// All Firestore fields related to alerts configuration
 /// Used for auth recovery and local backup restoration
